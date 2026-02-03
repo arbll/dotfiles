981cd0d91c51460abd5c50a629c7d13abc200137
Arthur Bellal <bellal.arthur@gmail.com>
2026-02-03 14:48:29 +0100

    Add HATCH_PYTHON env var for Datadog integration-core formatting fix

--- dot_config/fish/conf.d/env.fish
+++ dot_config/fish/conf.d/env.fish
@@ -3,5 +3,11 @@ set -gx EDITOR nvim
 set -gx PAGER less
 set -gx MANPAGER 'nvim +Man!'
 
+# Datadog-specific environment variables
+if test -d "$HOME/dd"
+    # Hack to fix formatting issues in integration-core
+    set -gx HATCH_PYTHON python3.13
+end
+
 fish_add_path -g "$HOME/.local/bin"
 fish_add_path -g "$HOME/.local/go/bin"
